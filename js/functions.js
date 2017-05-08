22725584ad43715fcbaf4ae04ba0356598d64fac
Malgorzata Rita <malgorzata.lyczywek@gmail.com>
2017-05-08 14:43:34 +0200

    final

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+$('.selectpicker').on('changed.bs.select', function(e) {
+  var temp = $(this).closest('.bootstrap-select');
+  temp.addClass("success");
+  $('p#warning').text('');
+  $('p#warning').removeAttr('id');
+  var button = temp.children('button');
+  var selectedMonth = button.attr("title");
+  $("#month").remove();
+  button.prepend('<span id="month">' + selectedMonth + '</span>');
+  button.children('span.filter-option').text("Miesiąc");
+});
+$('.selectpicker').on('shown.bs.select', function(e) {
+  $(this).closest('.bootstrap-select').addClass("error");
+  $('p#warning').text('Wybierz miesiąc');
+});
+//wlacz-wylacz
+$('.ex-disable').click(function() {
+  var button = $('.selectpicker').closest('.bootstrap-select').find('button');
+  var html = button.html();
+  $('select.example').prop('disabled', true);
+  $('p#warning').text('');
+  $('.bootstrap-select').removeClass('error');
+  $('select.example').selectpicker('refresh');
+  button.html(html);
+});
+
+$('.ex-enable').click(function() {
+  var button = $('.selectpicker').closest('.bootstrap-select').find('button');
+  var html = button.html();
+  $('.example').prop('disabled', false);
+  $('.example').selectpicker('refresh');
+  button.html(html);
+});
